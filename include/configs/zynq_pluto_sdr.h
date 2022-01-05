8ffeb684d7f2bc13f47eb66d3c12aa8777a4cedf
pbeltram <primoz.beltram@gmail.com>
2022-01-05 10:46:35 +0100

    pluto: Update build config files.

--- include/configs/zynq_pluto_sdr.h
+++ include/configs/zynq_pluto_sdr.h
@@ -2,28 +2,19 @@
 #ifndef __CONFIG_ZYNQ_PLUTO_SDR_H
 #define __CONFIG_ZYNQ_PLUTO_SDR_H
 
-//#define CONFIG_ZYNQ_I2C0
-//#define CONFIG_ZYNQ_EEPROM
+#include <configs/zynq-common.h>
 
 /*platform-auto.h include start*/
 
 /*platform-auto.h include stop*/
 
-#include <configs/zynq-common.h>
-
 #ifdef CONFIG_PREBOOT
 #undef CONFIG_PREBOOT
 #endif
-#define CONFIG_PREBOOT	"echo U-BOOT for Pluto SDR; pluto_sdr_init;" \
-	"echo importing env from FSBL shared area at 0xFFFFFC00; " \
-	"if itest *0xFFFFFC00 == 0xCAFEBABE; then echo Found valid magic; " \
-	"env import -t 0xFFFFFC04; fi;setenv preboot; echo; dhcp"
-
-/* BOOTCOMMAND */
-#ifdef CONFIG_BOOTCOMMAND
-#undef CONFIG_BOOTCOMMAND
-#endif
-#define CONFIG_BOOTCOMMAND	"run default_bootcmd"
+#define CONFIG_PREBOOT "echo U-BOOT for Pluto SDR; pluto_sdr_init;" \
+  "echo Look for ENV from FSBL shared area at 0xFFFFFC00; " \
+  "if itest *0xFFFFFC00 == 0xCAFEBABE; then echo Found valid ENV FSBL magic; " \
+  "env import -t 0xFFFFFC04; fi;setenv preboot; echo; dhcp"
 
 #endif /* __CONFIG_ZYNQ_PLUTO_SDR_H */
 
